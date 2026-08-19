72107e178203a23c1c0e110ab6d18eb060432c4d
ElfaLunar <luana.borini@escola.pr.gov.br>
2026-08-19 17:54:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let banner = document.querySelector('.banner');
-banner.onmousemove = function(e){
-  let x = e.pageX - banner.offsetLeft;
-  let y = e.pageY - banner.offsetTop;
-  banner.style.setProperty('--x', x + 'px');
-  banner.style.setProperty('--y', y + 'px');
-  }
+const banner = document.querySelector('.banner');
+
+banner.addEventListener('mousemove', function(e) {
+    const rect = banner.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    banner.style.setProperty('--x', `${x}px`);
+    banner.style.setProperty('--y', `${y}px`);
+});
